84fc57cd2941e22d86835591dc9ccb7efd475898
Wanderson Gonçalves <wgmbusinessintelligence@gmail.com>
2023-01-05 15:40:56 -0300

    Função adicionar linha

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,22 +1,48 @@
 
 var btnDispesaFixa = $("#adicionarDispesasFixas");
 
+
 $(function () {
     inserirDispesaFixa();
-    
+
 });
 
 function inserirDispesaFixa() {
     btnDispesaFixa.click(() => {
-       
-        let nomeDespesa = $("#nomeDespesa").text();
-        let valorDespesa = $("#valorDespesa").text();
-        let mostra = nomeDespesa;
-        console.log(mostra);
-        console.log(valorDespesa);
+        let nomeDespesa = $("#nomeDespesa").val();
+        let valorDespesa = $("#valorDespesa").val();
+
+        inserirConta(nomeDespesa, valorDespesa);
+
+
+
         event.preventDefault();
 
 
     });
 
+}
+
+function inserirConta(x, y) {
+    
+    let corpoTabela = $("#contasFixas").find("tbody");
+
+    let nome = x;
+    let valor = y;
+
+    let linha = novaLinha(nome, valor);
+
+    corpoTabela.append(linha);
+}
+
+function novaLinha(nomeConta, valorConta) {
+
+    let linha = $("<tr>");
+    let colunaNomeConta = $("<th>").text(nomeConta);
+    let colunaValorConta = $("<td>").text(valorConta);
+
+    linha.append(colunaNomeConta);
+    linha.append(colunaValorConta);
+
+    return linha;
 }
